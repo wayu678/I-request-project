8c974c305d7d327dce67e9cceadb8235c82cb5c5
wayu678 <151804609+wayu678@users.noreply.github.com>
2025-10-08 21:07:59 +0700

    add create request page

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,14 @@ import {
 import MakeUpExamForm from "./components/MakeUpExamForm";
 
 import { Demo } from "./pages/demo";
+import CreateRequest from "./pages/demo/createRequest";
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Demo />} />
       <Route path="/demo" element={<Demo />} />
+      <Route path="/demo/createRequest" element={<CreateRequest />} />
       <Route path="/login" element={<Login />} />
       <Route path="/make-up-exam" element={<MakeUpExamForm />} />
       <Route path="/irst07">
