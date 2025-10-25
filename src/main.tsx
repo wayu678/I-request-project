d2c06c9b5185595a4418a71b35c2481a8e19203c
Arpasiri <arpasiri.se@ku.th>
2025-10-25 16:00:11 +0700

    Resolve merge conflicts: update dashboard styling and remove animate.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import './animate.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import TranslateProvider from './provider/provider/translate.provider.tsx'
